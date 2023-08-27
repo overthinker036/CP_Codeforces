6e92c3fd2f204a17231ae7684832fa73f221d030
Omar Ibne Alam <83441944+Omar035@users.noreply.github.com>
2023-08-27 22:05:00 +0600

    486A in C# as well

---
+++ 486A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeforcesSolution {
+    class Program {
+        static void Main () {
+            Int64 x = Convert.ToInt64(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine(x/2);
+            } else {
+                Console.WriteLine(-(x - (x/2)));
+            }
+        }
+    }
+}
