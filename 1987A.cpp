f54582d4d2a4443d87c33bf1f840f4fa4d809722
Omar Ibne Alam <83441944+overthinker036@users.noreply.github.com>
2024-07-17 19:16:14 +0600

    Create 1987A.cpp

---
+++ 1987A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define pb push_back
+#define umap unordered_map
+#define f(i, x, n) for(ll i = x; i < n; i++)
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+ 
+int main() {
+    optimize();
+    
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        
+        cout << (n*k)-(k-1) << endl;
+    }
+}
