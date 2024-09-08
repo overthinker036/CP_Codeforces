64c8682dcd164e0b37409e79740ceea047f1cebd
Omar Ibne Alam <83441944+overthinker036@users.noreply.github.com>
2024-09-08 14:14:05 +0600

    Update

---
+++ 2010B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define ull unsigned long long
+#define umap unordered_map
+#define f(i, x, n) for (ll i = x; i < n; i++)
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main() {
+	optimize();
+	
+	int x, y;
+	cin >> x >> y;
+	cout << 6 - (x+y) << endl;
+}
